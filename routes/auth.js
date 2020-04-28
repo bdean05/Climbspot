09aff1510778e9ff662d98ec7eeb7592b0afd94b
OBxel <olivier.a.brule@gmail.com>
2020-04-28 13:11:44 +0200

    app.js auth.js signup.hbs up

--- routes/auth.js
+++ routes/auth.js
@@ -3,54 +3,28 @@ const router = new express.Router();
 
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-const { Router } = require("express");
-
-// const salt = bcrypt.genSaltSync(bcryptSalt);
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
-});
 
 router.get("/signin", (req, res, next) => {
   res.render("auth/signin", {
-    errorMessage: "error"
-  });
-});
-
-router.post("/signup", (req, res) => {
-  const { username, email, password } = req.body;
-  User.findOne({ username: username }).then(user => {
-    if (user !== null) {
-      res.render("/auth/signup", {
-        errorMessage: "the username already exists!"
-      });
-    } else {
-      const salt = 10;
-      const hashPass = bcrypt.hashSync(password, salt);
-      const newUser = {
-        username,
-        password: hashPass
-      };
-      User.create(newUser)
-        .then(res => {
-          res.redirect("/auth/signin");
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
+    errorMessage: "error",
   });
 });
 
 router.post("/signin", (req, res) => {
-  const { email, password } = req.body;
+  const {
+    email,
+    password
+  } = req.body;
   user
-    .foundOne({ email: email })
-    .then(foundUser => {
+    .foundOne({
+      email: email,
+    })
+    .then((foundUser) => {
       if (!foundUser) {
         errorMessage: "Error, invalid credentials!";
         res.redirect("/auth/signin");
-      } else {
+      }
+      else {
         if (bcrypt.compareSync(password, foundUser.password)) {
           req.session.currentUser = foundUser;
           res.redirect("/");
@@ -60,13 +34,53 @@ router.post("/signin", (req, res) => {
         }
       }
     })
-    .catch(dbErr => {
+    .catch((dbErr) => {
       console.log(dbErr);
     });
 });
 
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res) => {
+  console.log("heyheyehey");
+  const {
+    username,
+    password
+  } = req.body;
+  console.log(req.body);
+  User.findOne({
+      username: username,
+    }).then((user) => {
+      console.log("TOTO");
+      if (user) {
+        res.render("auth/signup", {
+          errorMessage: "the username already exists!",
+        });
+      } else {
+        console.log("hey i'm here");
+        const salt = 10;
+        const hashPass = bcrypt.hashSync(password, salt);
+        const newUser = {
+          username,
+          password: hashPass,
+        };
+        User.create(newUser)
+          .then((dbSuccess) => {
+            console.log("Im HERE");
+            res.redirect("/");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    })
+    .catch(err => console.log(err))
+});
+
 router.get("/logout", (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     res.redirect("/");
   });
 });
