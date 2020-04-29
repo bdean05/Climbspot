09f2902e65b8b27cb9e9343e0dcd588cbabd5752
OBxel <olivier.a.brule@gmail.com>
2020-04-29 10:24:56 +0200

    Added public and result

---
+++ routes/result.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = new express.Router();
+const Spot = require("../models/Spot");
+
+router.get('/result/:id', (req, res, next) => {
+    Spot.findById(req.params.id)
+        .then(searchresultdb => {
+            res.render('result', {
+                searchresultdb,
+                scripts: ["oneSpotMap.js"]
+            })
+        })
+})
+
+
+module.exports = router;
