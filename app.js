1987b772f25b47fa7b1bed7d69e22bdd3aa0bc58
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-04-29 14:32:43 +0200

    teddy
    map appears in result page

--- app.js
+++ app.js
@@ -44,18 +44,19 @@ const indexRouter = require("./routes/index");
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/", indexRouter);
+app.use('/', require('./routes/result'));
 
 app.locals.site_url = process.env.SITE_URL;
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
