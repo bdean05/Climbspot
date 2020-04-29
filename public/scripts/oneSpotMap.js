b5b5f3e8f58072b2e39030f200dea378ca537e2e
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-04-29 12:15:04 +0200

    Update oneSpotMap.js
    working on oneSpotMap.....

--- public/scripts/oneSpotMap.js
+++ public/scripts/oneSpotMap.js
@@ -1,3 +1,27 @@
+// function initMap() {
+//     // The location of Uluru
+//     var uluru = {
+//         lat: -25.344,
+//         lng: 131.036
+//     };
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {
+//             zoom: 4,
+//             center: uluru
+//         });
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({
+//         position: uluru,
+//         map: map
+//     });
+// }
+
+// initMap()
+
+
+// ---------------------------
+
 const mapCenter = {
     lat: 47.824905,
     lng: 2.618787
@@ -14,21 +38,20 @@ function initMapOneSpot() {
         });
 
     // The marker, positioned at spotMarker
-    axios.get("/api/spots/:id")
-        .then(apiRes => {
-            const spots = apiRes.data;
-            spots.forEach(spot => {
-                var marker = new google.maps.Marker({
-                    position: {
-                        lat: spot.latitude,
-                        lng: spot.longitude
-                    },
-                    map: map
-                });
-            })
-        }).catch(apiErr => {
-            console.log(apiErr)
+    axios.get("/api/spots/").then(apiRes => {
+        const spots = apiRes.data;
+        spots.forEach(spot => {
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: spot.latitude,
+                    lng: spot.longitude
+                },
+                map: map
+            });
         })
+    }).catch(apiErr => {
+        console.log(apiErr)
+    })
     console.log(spotMarker)
 }
 
