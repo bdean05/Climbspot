876a82f9d949bf68de1b29ceb572c2493119b447
OBxel <olivier.a.brule@gmail.com>
2020-04-30 14:45:35 +0200

    auth.js index.js propre

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,13 @@ var router = new express.Router();
 const Spot = require("../models/Spot");
 
 router.get("/", (req, res) => {
-  console.log(req.query);
   const inputQuery = req.query.search;
-
   Spot.find({
     name: {
       $regex: inputQuery || "default",
       $options: "im",
     },
   }).then((dbResult) => {
-    console.log(dbResult);
     res.render("index", {
       spotresult: dbResult,
       title: "Express",
