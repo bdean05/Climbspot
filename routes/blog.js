3341eb56a0273461e30395c704920aa2019a3cad
OBxel <olivier.a.brule@gmail.com>
2020-04-30 12:44:25 +0200

    blog.js blog.hbs fixed

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/blog", (req, res, next) => {
-    res.render("blog/blog", {
+    res.render("blog", {
         errorMessage: "error"
     });
 });
