e56cd9c3c02a9ca5476c771d1d3f2e01055ee839
OBxel <olivier.a.brule@gmail.com>
2020-04-30 12:41:35 +0200

    app.js blog route add

--- app.js
+++ app.js
@@ -84,18 +84,19 @@ app.use("/", indexRouter);
 app.use("/", require("./routes/spots"));
 app.use("/", require("./routes/result"));
 app.use("/", require("./routes/about"));
+app.use("/", require("./routes/blog"));
 
 app.locals.site_url = process.env.SITE_URL;
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
